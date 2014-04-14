ca006e7780c0ef602e35a0f4a469f41c62227fe3
Dvergar <laurent.rd@gmail.com>
2014-04-14 23:24:43 +0200

    simplified sample 3, fixed timer compatibility
    ADDED triangle shape
    FIXED addRandomShape timer
    FIXED collision resolution by pair
    REMOVED fps counter
    REMOVED entity number
    
    Tried to focus a bit more the sample on collision
    resolution.
    
    Added triangles and some rather interesting
    jitterings appeared.
    
    Also now that html5 target is *working* i can
    see how broken the whole sample became.

--- hxcollision/tests/usage3/Main.hx
+++ hxcollision/tests/usage3/Main.hx
@@ -19,14 +19,16 @@ class Main extends Sprite {
     var drawer : OpenFLDrawer;
     var visualise : Sprite;
 
+    var last_add_time : Float = haxe.Timer.stamp();
+
         //Shows number of entities
     var caption : flash.text.TextField;
 
         //Shapes/speed containers
     var allshapes_list : Array<Shape> = new Array();
     var dynamic_list : Array<Shape> = new Array();
+    var static_list : Array<Shape> = new Array();
     var speeds : Array<Float> = new Array();
-
 
     public function new() {
 
@@ -42,17 +44,20 @@ class Main extends Sprite {
         visualise = new Sprite();
             //the shape drawer
         drawer = new OpenFLDrawer( visualise.graphics );
+
             //add to the stage so we can see it
         addChild( visualise );
 
             //Create the static collider shapes
         var box = Polygon.rectangle( 140, 80, 150, 50 );
         box.rotation = 20;
-        allshapes_list.push( box );
-        allshapes_list.push( Polygon.create( 320,100, 6, 50 ) );
-        allshapes_list.push( new Circle( 350, 400, 50 ) );
-        allshapes_list.push( Polygon.create( 220,300, 9,60 ) );
-        allshapes_list.push( new Circle( 150, 450, 50 ) );
+        static_list.push( box );
+        static_list.push( Polygon.create( 320,100, 6, 50 ) );
+        static_list.push( new Circle( 350, 400, 50 ) );
+        static_list.push( Polygon.create( 220,300, 9,60 ) );
+        static_list.push( new Circle( 150, 450, 50 ) );
+
+        allshapes_list = static_list.copy();
 
             //caption
         var inputFormat = new flash.text.TextFormat();
@@ -63,18 +68,8 @@ class Main extends Sprite {
         caption.autoSize = flash.text.TextFieldAutoSize.LEFT;
         caption.mouseEnabled = false;
         caption.width = stage.stageWidth;
+        caption.text = "COLLISION RESOLUTION STRESS TEST";
         stage.addChild( caption );
-
-            //theming the fps counter
-        var fps = new openfl.display.FPS();
-        fps.x = 0; fps.y += 2;
-        fps.defaultTextFormat = inputFormat;
-        stage.addChild( fps );
-
-            //timer to spawn dynamic collider shapes at the top of the screen
-        var shape_timer:flash.utils.Timer = new flash.utils.Timer(500, 0);
-        shape_timer.addEventListener( flash.events.TimerEvent.TIMER, addRandomShape );
-        shape_timer.start();
 
             //And finally, listen for updates.
         addEventListener( Event.ENTER_FRAME, update );
@@ -82,37 +77,48 @@ class Main extends Sprite {
     } //construct
 
         //Add random shapes from top of the stage
-    function addRandomShape( e:flash.events.TimerEvent ) {
+    function addRandomShape( ) {
 
         var shape : Shape;
         var size : Int;
         var start_x : Int;
+        var start_y : Int;
+        var random_shapes : Array<Shape>;
 
         size = Std.random(15) + 10;
         start_x = 90;
+        start_y = -50;
 
-            //randomly spawn Circle or Polygon
-        (Std.random(2) == 0) ?
-            shape = new Circle( start_x,-50, size ):
-            shape = Polygon.create( start_x,-50, 9,size );
+            //specify shapes to be randomly spawned
+        random_shapes = [new Circle( start_x, start_y, size ),
+                         Polygon.create( start_x, start_y, 9, size ),
+                         Polygon.triangle( start_x, start_y, size )];
+
+            //randomly select a shape
+        shape = random_shapes[Std.random(3)];
 
         dynamic_list.push( shape );
         allshapes_list.push( shape );
         speeds.push( Std.random(6) + 1 );
-    }
+
+    } //addRandomShape
 
     var end_dt : Float = 0;
     var dt : Float = 0;
 
     public function update( e:Event ) {
 
+        var results : Array<CollisionData>;
+        var shape : Shape;
+
         dt = haxe.Timer.stamp() - end_dt;
         end_dt = haxe.Timer.stamp();
 
-        var shape : Shape;
-
-            //update number of entities in caption
-        caption.text = "STRESS TEST : " + dynamic_list.length + " shapes";
+            //timer to spawn dynamic shapes at the top of the screen
+        if(haxe.Timer.stamp() - last_add_time > 1) {
+            addRandomShape();
+            last_add_time = haxe.Timer.stamp();
+        }
 
             //collision handling
         var i = dynamic_list.length;
@@ -120,7 +126,8 @@ class Main extends Sprite {
             shape = dynamic_list[i];
             shape.y += speeds[i];
 
-            var results = Collision.testShapeList( shape, allshapes_list );
+                //collision against other moving shapes
+            results = Collision.testShapeList( shape, dynamic_list );
 
             for(result in results)
             {
@@ -128,15 +135,27 @@ class Main extends Sprite {
                 if(result.shape2 == shape) continue;
 
                     //applies correction
+                shape.x += result.separation.x / 2;
+                shape.y += result.separation.y / 2;
+
+                result.shape2.x -= result.separation.x / 2;
+                result.shape2.y -= result.separation.y / 2;
+            }
+
+                //collision against static shapes
+            results = Collision.testShapeList( shape, static_list );
+
+            for(result in results) {
+                    //applies correction
                 shape.x += result.separation.x;
                 shape.y += result.separation.y;
-
             }
 
                 //remove the shape if outside of the viewport
             if(shape.y > stage.stageHeight + 50) {
                 dynamic_list.remove(shape);
                 allshapes_list.remove(shape);
+                speeds.remove(speeds[i]);
             }
         }
     
