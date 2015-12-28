509d449a3e3424133715e9f287e6d9970e8ed4ab
underscorediscovery <sven@underscorediscovery.com>
2015-12-28 14:46:04 -0330

    ShapeCollision; flatten to floats, add a reset and clone function (for later fixes)

--- differ/data/ShapeCollision.hx
+++ differ/data/ShapeCollision.hx
@@ -9,22 +9,38 @@ class ShapeCollision {
 
         /** the overlap amount */
     public var overlap : Float = 0;
-        /** a vector that when subtracted to shape 1 will separate it from shape 2 */
-    public var separation : Vector;
+        /** y component of the separation vector, when subtracted to shape 1 will separate it from shape 2 */
+    public var separationX : Float;
+        /** y component of the separation vector, when subtracted to shape 1 will separate it from shape 2 */
+    public var separationY : Float;
 
         /** the first shape */
     public var shape1 : Shape;
         /** the second shape */
     public var shape2 : Shape;
-        /** unit vector on the axis of the collision (the normal of the face that was collided with) */
-    public var unitVector : Vector;
+        /** x value for unit vector on the axis of the collision (the normal of the face that was collided with) */
+    public var unitVectorX : Float;
+        /** y value for unit vector on the axis of the collision (the normal of the face that was collided with) */
+    public var unitVectorY : Float;
 
     @:noCompletion
-    public function new() {
+    public function new() {}
 
-        separation = new Vector();
-        unitVector = new Vector();
+    public inline function reset() {
+        separationX = separationY = overlap = unitVectorX = unitVectorY = 0.0;
+        shape1 = shape2 = null;
+    }
 
-    } //new
+    public inline function clone() {
+        var c = new ShapeCollision();
+        c.overlap = overlap;
+        c.separationX = separationX;
+        c.separationY = separationY;
+        c.shape1 = shape1;
+        c.shape2 = shape2;
+        c.unitVectorX = unitVectorX;
+        c.unitVectorY = unitVectorY;
+        return c;
+    }
 
 } //ShapeCollision
