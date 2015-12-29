a2c7eefa1346d5ec70863ba325a64d9fd2e13c93
underscorediscovery <sven@underscorediscovery.com>
2015-12-29 19:27:20 -0330

    ShapeCollision; add "other" collision info for polygons, add copy_from

--- differ/data/ShapeCollision.hx
+++ differ/data/ShapeCollision.hx
@@ -5,42 +5,82 @@ import differ.shapes.*;
 import differ.data.*;
 
 /** Collision data, obtained by testing two shapes for a collision. */
+@:publicFields
 class ShapeCollision {
 
-        /** the overlap amount */
-    public var overlap : Float = 0;
-        /** y component of the separation vector, when subtracted to shape 1 will separate it from shape 2 */
-    public var separationX : Float;
-        /** y component of the separation vector, when subtracted to shape 1 will separate it from shape 2 */
-    public var separationY : Float;
+        /** The overlap amount */
+    var overlap : Float = 0.0;
+        /** X component of the separation vector, when subtracted from shape 1 will separate it from shape 2 */
+    var separationX : Float = 0.0;
+        /** Y component of the separation vector, when subtracted from shape 1 will separate it from shape 2 */
+    var separationY : Float = 0.0;
+        /** X component of the unit vector, on the axis of the collision (i.e the normal of the face that was collided with) */
+    var unitVectorX : Float = 0.0;
+        /** Y component of the unit vector, on the axis of the collision (i.e the normal of the face that was collided with) */
+    var unitVectorY : Float = 0.0;
 
-        /** the first shape */
-    public var shape1 : Shape;
-        /** the second shape */
-    public var shape2 : Shape;
-        /** x value for unit vector on the axis of the collision (the normal of the face that was collided with) */
-    public var unitVectorX : Float;
-        /** y value for unit vector on the axis of the collision (the normal of the face that was collided with) */
-    public var unitVectorY : Float;
+    var otherOverlap : Float = 0.0;
+    var otherSeparationX : Float = 0.0;
+    var otherSeparationY : Float = 0.0;
+    var otherUnitVectorX : Float = 0.0;
+    var otherUnitVectorY : Float = 0.0;
+
+        /** The shape that was tested */
+    var shape1 : Shape;
+        /** The shape that shape1 was tested against */
+    var shape2 : Shape;
 
     @:noCompletion
-    public function new() {}
+    inline function new() {
 
-    public inline function reset() {
-        separationX = separationY = overlap = unitVectorX = unitVectorY = 0.0;
+    } //
+
+    inline function reset() {
+
         shape1 = shape2 = null;
-    }
+        overlap = separationX = separationY = unitVectorX = unitVectorY = 0.0;
+        otherOverlap = otherSeparationX = otherSeparationY = otherUnitVectorX = otherUnitVectorY = 0.0;
+        
+        return this;
 
-    public inline function clone() {
-        var c = new ShapeCollision();
-        c.overlap = overlap;
-        c.separationX = separationX;
-        c.separationY = separationY;
-        c.shape1 = shape1;
-        c.shape2 = shape2;
-        c.unitVectorX = unitVectorX;
-        c.unitVectorY = unitVectorY;
-        return c;
-    }
+    } //reset
+
+    inline function clone() {
+
+        var _clone = new ShapeCollision();
+
+            _clone.overlap = overlap;
+            _clone.separationX = separationX;
+            _clone.separationY = separationY;
+            _clone.unitVectorX = unitVectorX;
+            _clone.unitVectorY = unitVectorY;
+            _clone.otherOverlap = otherOverlap;
+            _clone.otherSeparationX = otherSeparationX;
+            _clone.otherSeparationY = otherSeparationY;
+            _clone.otherUnitVectorX = otherUnitVectorX;
+            _clone.otherUnitVectorY = otherUnitVectorY;
+            _clone.shape1 = shape1;
+            _clone.shape2 = shape2;
+
+        return _clone;
+
+    } //clone
+
+    inline function copy_from(_other:ShapeCollision) {
+
+        overlap = _other.overlap;
+        separationX = _other.separationX;
+        separationY = _other.separationY;
+        unitVectorX = _other.unitVectorX;
+        unitVectorY = _other.unitVectorY;
+        otherOverlap = _other.otherOverlap;
+        otherSeparationX = _other.otherSeparationX;
+        otherSeparationY = _other.otherSeparationY;
+        otherUnitVectorX = _other.otherUnitVectorX;
+        otherUnitVectorY = _other.otherUnitVectorY;
+        shape1 = _other.shape1;
+        shape2 = _other.shape2;
+
+    } //copy_from
 
 } //ShapeCollision
